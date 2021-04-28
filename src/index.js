091c81f6dcd673c80d534e8a5aa6f34dd01125bb
Mateo Ceballos Bermudez <mat.bermudez@udla.edu.co>
2021-04-28 03:07:27 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+(bodyParser = require("body-parser")),
+  (mongoose = require("mongoose")),
+  ({ DB } = require("./config/DB")),
+  (itemRoutes = require("./routes/item"));
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(DB, { useMongoClient: true })
+  .then(() => console.log("Db is conencted"))
+  .catch((err) => console.error(err));
+
+const app = express();
+var port = process.env.PORT || 4000;
+
+// middlewares
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+// routes
+app.use("/items", itemRoutes);
+
+// start the server
+var server = app.listen(port, function () {
+  console.log("Listening on port " + port);
+});
